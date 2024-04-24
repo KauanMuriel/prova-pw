8dbb2b39c62665367cc68ef6a7d20cb6eb07f5e1
kauanrossii <107654514+KauanMuriel@users.noreply.github.com>
2024-04-24 23:28:58 +0000

    Pagina principal finalizada

---
+++ assets/js/index.js
@@ -0,0 +1,20 @@
+window.onload = async () => {
+    const listElement = document.querySelector('#state-list');
+
+    try {
+        const response = await fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados');
+        const states = await response.json();
+        states.forEach(insertStateInList);
+    }catch(error) {
+        console.error(error)
+    }
+
+    function insertStateInList(state) {
+        const anchorElement = document.createElement('a');
+        anchorElement.textContent = state.nome;
+        anchorElement.href = `./municipios/index.html?uf=${state.nome}&id=${state.id}`;
+        const itemElement = document.createElement('li');
+        itemElement.appendChild(anchorElement);
+        listElement.appendChild(itemElement);
+    }
+}
